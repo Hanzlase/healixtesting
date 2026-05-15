35efaa04a3621894d0e5616af205f0d0399e2c18
Muhammad Hunzla <Hanzlasabir309@gmail.com>
2026-05-15 07:19:12 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const calculatorRouter = require('./routes/calculator');
 
-const app = express();
+const app = expr
 const PORT = process.env.PORT || 3000;
 
 // Middleware
